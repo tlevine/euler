735a585e5885c3e34073e91529483939627dc0e1
Thomas Levine <occurrence@thomaslevine.com>
2013-02-14 20:46:56 -0500

    integer

--- 413.hs
+++ 413.hs
@@ -4,8 +4,9 @@ import qualified Data.Text as T
 --subStrings :: Integer -> S.Set Integer
 --subStrings i = map (\s -> read s :: Integer) subStringsStr
 
-subStringsStr :: Integer -> S.Set T.Text
-subStringsStr i = S.fromList $ T.chunksOf 2 $ T.pack $ show i
+
+subStrings :: Integer -> Int -> S.Set Integer
+subStrings i n = S.map (\i' -> read (T.unpack i') :: Integer) $ S.fromList $ T.chunksOf n $ T.pack $ show i
 
 main = do
-  putStrLn $ show $ subStringsStr 1234
+  putStrLn $ show $ subStrings 1234 2
