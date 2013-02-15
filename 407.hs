599ae79b9dc543e8bf5c5b1c203971115c4b1e4f
Thomas Levine <occurrence@thomaslevine.com>
2013-02-14 20:07:13 -0500

    word64

---
+++ 407.hs
@@ -0,0 +1,22 @@
+--If we calculate a^2 mod 6 for 0  a  5 we get: 0,1,4,3,4,1.
+
+--The largest value of a such that a^2 = a mod 6 is 4.
+--Let's call M(n) the largest value of a < n such that a^2 = a (mod n).
+--So M(6) = 4.
+
+--Find M(n) for 1 <= n <= 10^7.
+
+import Data.Word
+
+m :: Word64 -> (Word64, Word64)
+--m n = fst $ head $ filter (\pair -> fst pair == snd pair ) $ reverse $ equation n
+m n = head $ reverse $ equation n
+
+equation :: Word64 -> [(Word64,Word64)]
+equation n = zip left right
+  where
+    left = map (\a -> a^2) [0..n]
+    right = map (\a -> a `mod` n) [0..n]
+
+main = do
+  putStrLn $ show $ m 3
