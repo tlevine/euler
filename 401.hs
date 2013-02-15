c65e5a416d230277f9c7bec3a3265eeefed49629
Thomas Levine <occurrence@thomaslevine.com>
2013-02-15 17:46:32 -0500

    aoeu

--- 401.hs
+++ 401.hs
@@ -1,6 +1,5 @@
 divisors :: Integer -> [Integer]
-divisors 1 = [1]
-divisors n = (1 : filter ((==0) . rem n) [2 .. n `div` 2]) ++ [n]
+divisors n = filter ((==0) . rem n) [1..n]
 
 sigma2 :: Integer -> Integer
 sigma2 n = sum $ map (^2) $ divisors n
@@ -8,10 +7,8 @@ sigma2 n = sum $ map (^2) $ divisors n
 sigma2' :: Integer -> Integer
 sigma2' n = sum $ map sigma2 [1..n]
 
-
-
-
 main = do
   putStrLn $ show $ sigma2 6
   putStrLn $ show $ [1,6,16,37,63,113]
   putStrLn $ show $ map sigma2' [1..6]
+  putStrLn $ show $ (sigma2' 10^15) `mod` 10^9
