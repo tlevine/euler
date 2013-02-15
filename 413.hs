d23265f9009e90190703c6a49c23794fb6ccc426
Thomas Levine <occurrence@thomaslevine.com>
2013-02-14 20:55:13 -0500

    substrings

--- 413.hs
+++ 413.hs
@@ -11,11 +11,11 @@ subStringsN i n
   | otherwise = S.map (\i' -> read (T.unpack i') :: Integer) $ S.fromList $ T.chunksOf n $ T.pack $ show i
 
 subStrings :: Integer -> S.Set Integer
-subStrings i = S.fromList []
+subStrings i = S.unions $ map s [1..d]
   where
     -- Number of digits
     d = length $ show i
     s = subStringsN i
 
 main = do
-  putStrLn $ show $ subStringsN 1234 (-3)
+  putStrLn $ show $ subStrings 1234 
