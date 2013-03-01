bf39fb651faf9da782b854a81442587b5fd65151
Thomas Levine <occurrence@thomaslevine.com>
2013-03-01 02:59:50 -0500

    yay

--- 30.hs
+++ 30.hs
@@ -4,14 +4,29 @@ import qualified Data.List as L
 convNumber :: [Integer] -> [Integer]
 convNumber number
   | (head number) < 10 = number
-  | otherwise          = convNumber $ (floor $ fromIntegral x) : (x `mod` 10) : (L.drop 1 number)
-  where
-    x = (head number) `div` 10
+  | otherwise          = convNumber $ (floor $ fromIntegral $ (head number) `div` 10) : ((head number) `mod` 10) : (tail number)
 
---isDigitFifthPower :: Integer -> Bool
---isDigitFifthPower number = 3
---  where
---    sumOfPowers = sum $ map (^5) []
+isDigitPower :: Integer -> Integer -> Bool
+isDigitPower exponent number = sumOfPowers == number
+  where
+    sumOfPowers = sum $ map (^ exponent) $ convNumber [number]
+
+
+
+-- Bounds
+--
+--  888 -> 3 * 8^5 = 2^15 * 3
+--   88 -> 2 * 8^5 = 2^16
+--  222 ->           2^5  * 3
+-- 1810 -> 2 + 32
+--
+-- Hmm. Well let's just choose a high number and hope that's enough.
+
 
 main = do
-  putStrLn $ show $ convNumber [38243]
+--putStrLn $ show $ convNumber [10000]
+--putStrLn $ show $ convNumber [18306]
+--putStrLn $ show $ convNumber [12306]
+--putStrLn $ show $ convNumber [32300]
+  putStrLn $ show $ filter (isDigitPower 4) [1..10000]
+  putStrLn $ show $ filter (isDigitPower 5) [1..1000000]
