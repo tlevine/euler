eb6c70ef34871115c2497f9975ad87eefd8fae10
Thomas Levine <occurrence@thomaslevine.com>
2013-03-01 02:41:46 -0500

    make string

--- 30.hs
+++ 30.hs
@@ -1,14 +1,17 @@
-import Data.Natural -- http://hackage.haskell.org/package/natural-numbers
+import qualified Data.List as L
 
-type Number = [Natural]
+-- Keep converting a number until it's ready.
+convNumber :: [Integer] -> [Integer]
+convNumber number
+  | (head number) < 10 = number
+  | otherwise          = convNumber $ (floor $ fromIntegral x) : (x `mod` 10) : (L.drop 1 number)
+  where
+    x = (head number) `div` 10
 
--- Memoized power function
---p :: Natural -> Natural
-p = (map (fromIntegral . (^500)) [0..] !!)
+--isDigitFifthPower :: Integer -> Bool
+--isDigitFifthPower number = 3
+--  where
+--    sumOfPowers = sum $ map (^5) []
 
 main = do
-  putStrLn $ show $ p 102
-  putStrLn $ show $ p 102
-  putStrLn $ show $ p 101
-  putStrLn $ show $ p 101
-  putStrLn $ show $ p 101
+  putStrLn $ show $ convNumber [38243]
