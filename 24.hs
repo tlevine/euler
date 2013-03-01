a0fff65d9aaee60f9212382c62b4ca3be0660827
Thomas Levine <occurrence@thomaslevine.com>
2013-03-01 00:21:27 -0500

    oeu

--- 24.hs
+++ 24.hs
@@ -1,4 +1,17 @@
--- There are 10! = 3 million combinations
--- If I fix the largest place at 9, there are 9! ~= 360,000 combinations
--- If I limit the largest place to 9, 8 or 7, there are 3 * 9! ~= 1.1 million combinations
+-- There are 10! = 3 million permutations
+-- If I fix the largest place at 9, there are 9! ~= 360,000 permutations
+-- If I limit the largest place to 9, 8 or 7, there are 3 * 9! ~= 1.1 million permutations
 -- Then I limit the second-largest place only when the first-largest place is 7...
+-- Keep doing this until I have exactly 1 million permutations.
+-- Then choose the largest permutation.
+
+type Restriction = (Integer, Integer)
+
+-- For a given set of restrictions, how many permutations are there?
+nPermutations :: Restriction -> Integer
+nPermutations (fullyFixedPlaces, partialDigits) = partialDigits * (unFixedPlaces ^ unFixedPlaces)
+  where
+    unFixedPlaces = 10 - fullyFixedPlaces
+
+main = do
+  putStrLn "3"
